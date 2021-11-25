06178a72cc2cd9832fd0db406671ace65fe5cfc3
raghu <raghugopal1695@gmail.com>
2021-11-25 20:19:12 +0530

    Toaster
    Html design done

--- B16BBookKart/src/app/app.module.ts
+++ B16BBookKart/src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
+import { ToasterMessageComponent } from './Message/toaster-message/toaster-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToasterMessageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,ToastrModule
   ],
   providers: [],
   bootstrap: [AppComponent]
